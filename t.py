a5893772f84ec299a6bdccdd64c713b71fc51cc9
Nadine Nam <94868412+dnam912@users.noreply.github.com>
2024-08-26 20:47:16 +0000

    added tokenizer.py

---
+++ t.py
@@ -0,0 +1,20 @@
+ls *.py
+ls fred.*
+
+[abcd]*
+
+# True - No red underline
+(1)
+((1))
+(((1)))
+
+# False - Red underline
+(z)
+
+
+(*[1234567890]+)*
+
+
+# Regular Expression-> "X"
+# "X+" 
+
--- topic-01-simple_expressions/tokenizer.py
+++ topic-01-simple_expressions/tokenizer.py
@@ -1,20 +1,66 @@
-ls *.py
-ls fred.*
+# tokenizer
 
-[abcd]*
+"""""
+break character stream into tokens, provide a token stream
+"""""
 
-# True - No red underline
-(1)
-((1))
-(((1)))
+import re
 
-# False - Red underline
-(z)
+patterns = [
+    ["\\(", "("],
+    ["\\)", ")"],
+    ["\\+", "+"],
+    ["\\-", "-"],
+    ["\\*", "*"],
+    ["\\/", "/"],
+    [
+        "(\\d+\\.\\d*)|(\\d*\.\\d+)|(\\d+)", "number"
+    ],
+]
 
+# string & regular spatial patterns
 
-(*[1234567890]+)*
+for pattern in patterns:
+    pattern[0] = re.compile(pattern[0])
 
+def tokenize(characters):
+    tokens = []
+    position = 0
 
-# Regular Expression-> "X"
-# "X+" 
+    while position < len(characters):
+        for pattern, tag in patterns:
+            match = pattern.match(characters, position)
+            if match:
+                break
+        assert match
+        #print("match found.", match)
+        token = {
+            'tag': tag,
+            'value': match.group[0],
+            'position': position, 
+        }
+        tokens.append(match)
+        position = match.end()
+    return tokens
 
+def test_simple_tokens():
+    print("testing simple tokens")
+    assert tokenize("+") == [{'tag': '+', 'value': '+', 'position': 0}]
+    assert tokenize("-") == [{'tag': '-', 'value': '-', 'position': 0}]
+    i = 0
+    for char in "+-*/()":
+        tokens = tokenize(char)
+        print(tokens)
+        assert tokens[0]["tag"] == char
+        assert tokens[0]["value"] == char
+        assert tokens[0]["position"] == i
+
+    for number in ["123.45"]:
+        tokens = tokenize(number)
+        assert tokens[0]["tag"] == "number"
+
+if __name__ == "__main__":
+    test_simple_tokens()
+    tokens = tokenize("123.45")
+    print(tokens)
+    print("done.")
